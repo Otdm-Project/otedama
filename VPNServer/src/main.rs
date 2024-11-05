f2cc9f67411bf2c6773a68134d83391c23c3318e
tomoya.otaki <nyarun.otaki@gmail.com>
2024-11-05 11:10:02 +0900

    VPNServerに監視設定を追加

--- VPNServer/src/main.rs
+++ VPNServer/src/main.rs
@@ -1,12 +1,14 @@
 mod db;
 mod handler;
 mod wireguard;
+mod monitoring;
 
 use warp::Filter;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+    // WebSocketサーバーを非同期タスクで起動
     let ws_route = warp::path("ws")
         .and(warp::ws())
         .map(|ws: warp::ws::Ws| {
@@ -15,5 +17,38 @@ async fn main() {
 
     let addr: SocketAddr = "0.0.0.0:8090".parse().expect("Unable to parse socket address");
     println!("VPNServer is running on {}", addr);
-    warp::serve(ws_route).run(addr).await;
+
+    tokio::spawn(async move {
+        warp::serve(ws_route).run(addr).await;
+    });
+
+    // monitoring関数を別スレッドで非同期タスクとして実行
+    tokio::task::spawn_blocking(|| {
+        println!("Starting monitoring...");
+        monitoring();
+    })
+    .await
+    .expect("Failed to run monitoring");
+
+    // メイン関数が終了しないように待機
+    tokio::signal::ctrl_c().await.expect("Failed to listen for ctrl+c");
+}
+
+// monitoring関数を呼び出すために定義
+fn monitoring() {
+    // サーバを別スレッドで起動
+    let server_handle = std::thread::spawn(|| {
+        println!("monitoring S start!");
+        monitoring::start_server();
+    });
+
+    // クライアントを起動
+    let client_handle = std::thread::spawn(|| {
+        println!("monitoring C start!");
+        monitoring::start_client();
+    });
+
+    // 両スレッドを終了まで待機
+    server_handle.join().unwrap();
+    client_handle.join().unwrap();
 }
--- VPNServer/src/monitoring.rs
+++ VPNServer/src/monitoring.rs
@@ -1,6 +1,7 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::time::Duration;
+use std::thread;
 
 // クライアント側: "Alive"メッセージを5秒ごとに送信
 pub fn start_client() {
@@ -9,7 +10,7 @@ pub fn start_client() {
             Ok(mut stream) => {
                 let message = "Alive";
                 stream.write_all(message.as_bytes()).unwrap();
-                println!("Sent: {} for APIServer ", message);
+                println!("Sent: {} for VPNServer ", message);
             }
             Err(e) => {
                 println!("Failed to connect: {}", e);
