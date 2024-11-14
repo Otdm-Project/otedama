ddbc240f061e3164b4e30e50db7a5895400ad23e
tomoya.otaki <nyarun.otaki@gmail.com>
2024-11-14 10:23:28 +0900

    サブドメインを5文字で生成し、末尾の文字からインクリメントするように設定

--- ProxyServer/src/subdomain.rs
+++ ProxyServer/src/subdomain.rs
@@ -2,10 +2,10 @@ use std::sync::Mutex;
 use std::io::Result;
 
 static DOMAIN: &str = "otdm.dev";
-static ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
+static CHARSET: &str = "abcdefghijklmnopqrstuvwxyz0123456789"; // アルファベットと数字の組み合わせ
 
 lazy_static::lazy_static! {
-    static ref SUBDOMAIN_COUNTER: Mutex<Vec<usize>> = Mutex::new(vec![0]);
+    static ref SUBDOMAIN_COUNTER: Mutex<Vec<usize>> = Mutex::new(vec![0; 5]); // 初期は5文字のカウンタ
 }
 
 // サブドメイン生成
@@ -14,17 +14,17 @@ pub fn generate_subdomain() -> Result<String> {
     let mut subdomain = String::new();
 
     for &index in counter.iter() {
-        subdomain.push(ALPHABET.chars().nth(index).unwrap());
+        subdomain.push(CHARSET.chars().nth(index).unwrap());
     }
 
     counter[0] += 1;
     let mut i = 0;
-    while i < counter.len() && counter[i] == ALPHABET.len() {
+    while i < counter.len() && counter[i] == CHARSET.len() {
         counter[i] = 0;
         if i + 1 < counter.len() {
             counter[i + 1] += 1;
         } else {
-            counter.push(0);
+            counter.push(0); // 必要に応じて文字数を増加
         }
         i += 1;
     }
