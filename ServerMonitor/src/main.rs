76216859ae8211d3caeba7e79a9282eb555e295f
tomoya.otaki <nyarun.otaki@gmail.com>
2024-11-19 14:45:17 +0900

    サーバ用の内容は監視側で設定するため削除

--- ServerMonitor/src/main.rs
+++ ServerMonitor/src/main.rs
@@ -20,39 +20,7 @@ fn start_client() {
     }
 }
 
-// サーバ側: メッセージを受信
-fn start_server() {
-    let listener = TcpListener::bind("127.0.0.1:2000").unwrap();
-    println!("Server is running on port 2000");
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buffer = [0; 1024];
-                match stream.read(&mut buffer) {
-                    Ok(size) => {
-                        if size > 0 {
-                            let received = String::from_utf8_lossy(&buffer[..size]);
-                            println!("Received: {}", received);
-                        }
-                    }
-                    Err(e) => {
-                        println!("Failed to receive data: {}", e);
-                    }
-                }
-            }
-            Err(e) => {
-                println!("Failed to accept connection: {}", e);
-            }
-        }
-    }
-}
-
 fn main() {
-    // サーバを別スレッドで起動
-    let server_handle = thread::spawn(|| {
-        start_server();
-    });
 
     // クライアントを起動
     let client_handle = thread::spawn(|| {
@@ -60,6 +28,5 @@ fn main() {
     });
 
     // 両スレッドを終了まで待機
-    server_handle.join().unwrap();
     client_handle.join().unwrap();
 }
