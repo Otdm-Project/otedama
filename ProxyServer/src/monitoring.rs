7150208106261a7ba3f61aa7681151a9c504e058
tomoya.otaki <nyaru.otaki@gmail.com>
2024-12-09 23:23:45 +0900

    Monitoring.rsを変更

--- ProxyServer/src/monitoring.rs
+++ ProxyServer/src/monitoring.rs
@@ -1,20 +1,22 @@
-use std::io::Write;
-use std::net::TcpStream; 
-use std::time::Duration;
-
 use std::thread;
+use std::io::Write;
+use std::net::TcpStream;
+use std::time::Duration;
+use tracing::{info, error};
 
-// クライアント側: "Alive"メッセージを5秒ごとに送信
 pub fn start_client() {
     loop {
         match TcpStream::connect("127.0.0.1:2000") {
             Ok(mut stream) => {
                 let message = "Alive";
-                stream.write_all(message.as_bytes()).unwrap();
-                println!("Sent: {} for APIServer ", message);
+                if let Err(e) = stream.write_all(message.as_bytes()) {
+                    error!("Failed to send message: {}", e);
+                } else {
+                    info!("Sent: {} for APIServer", message);
+                }
             }
             Err(e) => {
-                println!("Failed to connect: {}", e);
+                error!("Failed to connect: {}", e);
             }
         }
         thread::sleep(Duration::from_secs(5)); // 5秒ごとに送信
