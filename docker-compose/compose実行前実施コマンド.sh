4706cf231fad88856ff4431a70c41686dfec3ec1
maritos-net <nyarun.otaki@gmail.com>
2025-01-19 16:12:28 +0900

    コンテナ停止と削除に関わるコマンドを追記

--- docker-compose/compose実行前実施コマンド.sh
+++ docker-compose/compose実行前実施コマンド.sh
@@ -27,7 +27,16 @@ cd ~/proxyserver
 # 自分の端末で実行する場合には cd ~/otedama/VPNServer
 ```
 # ベースイメージの構築
-docker build --no-cache -f Dockerfile.base -t vpn_proxyimage:latest .
+docker build --no-cache -f Dockerfile.base -t proxy_baseimage:latest .
 # 本番イメージの構築
 docker build --no-cache -f Dockerfile -t proxy:v1.0 .
+```
+
+実行中コンテナ確認コマンド（停止中も含むには -a ）
+``` 
+docker ps 
+```
+これらの停止+削除コマンド
+```
+docker stop $(docker ps -a -q) && docker rm $(docker ps -aq)
 ```
