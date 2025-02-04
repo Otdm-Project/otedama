6cd24bae2909942f307f7f41d87ecd9dbbc79dbd
maritos-net <nyarun.otaki@gmail.com>
2025-02-04 13:53:03 +0900

    k8s化に伴いそれに必要なコマンドファイルを作成

---
+++ Management/containerテスト手順/Kopose実行準備.sh
@@ -0,0 +1,11 @@
+# APIコンテナ
+docker tag api:v1.0 maritosnet/api:v1.0
+docker push maritosnet/api:v1.0
+
+# VPNコンテナ
+docker tag vpn:v1.0 maritosnet/vpn:v1.0
+docker push maritosnet/vpn:v1.0
+
+# Proxyコンテナ
+docker tag proxy:v1.0 maritosnet/proxy:v1.0
+docker push maritosnet/proxy:v1.0
