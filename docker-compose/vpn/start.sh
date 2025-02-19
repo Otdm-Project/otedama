a97221944f57b34ce7f20d4fe44178468b05c0ac
ODENKITUNE <g021c1047@g.neec.ac.jp>
2025-02-19 15:38:33 +0000

    enable monitoring container

--- docker-compose/vpn/start.sh
+++ docker-compose/vpn/start.sh
@@ -2,6 +2,8 @@
 # IPv4フォワードを有効化
 # sysctl -w net.ipv4.ip_forward=1
 
+wg genkey | tee /etc/wireguard/privatekey > /dev/null && sed -i "s#PRIVKEY#$(cat /etc/wireguard/privatekey)#" /etc/wireguard/wg0.conf
+
 # WireGuardを起動
 wg-quick up /etc/wireguard/wg0.conf
 
